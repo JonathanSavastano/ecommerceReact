59eddede84df7e11b2fecd3ef0b7c8ccc85f42e6
Jonny Savastano <jonathan.savastano1246@gmail.com>
2024-04-04 17:53:13 -0600

    trying to get local to display content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
+// import necessary libraries
 import './App.css';
 import Product from './components/Products';
-import productCard from './components/ProductCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to my e-commerce website</h1>
+      <Product />
+      <ProductCard />
     </div>
   );
 }
