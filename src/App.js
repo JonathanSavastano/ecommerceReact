18a92a8f7d90a7fd5860b0fdd265cef8f086efde
Jonny Savastano <jonathan.savastano1246@gmail.com>
2024-04-05 09:48:48 -0600

    refactoring app.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
 // import necessary libraries
 import './App.css';
+import React from 'react';
 import Product from './components/Products';
 import Cart from './components/Cart';
 
 function App() {
-  const [cartItems, setCartItems] = useState([]);
-
-  function addToCart(product)
-  {
-    setCartItems(prevItems => [...prevItems, product]);
-  }
-
   return (
-    <Router>
-      <div className="App">
+    <div className="App">
       <h1>Welcome to my e-commerce website</h1>
-      <Link to="/cart">Go to Cart</Link>
-      <Switch>
-        <Route path="/cart">
-          <Cart cartItems={cartItems} />
-        </Route>
-        <Route path="/">
-          <Product addToCart={addToCart} />
-        </Route>
-      </Switch>
+      <Cart />
+      <Product />
     </div>
-    </Router>
   );
 }
 
