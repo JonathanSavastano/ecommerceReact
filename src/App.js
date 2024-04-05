26571ec481f4054d1eff1a50c2a81dc5d4650a14
Jonny Savastano <jonathan.savastano1246@gmail.com>
2024-04-05 10:16:10 -0600

    linked and fixed cart and checkout components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 // import necessary libraries
+import {useState} from 'react';
 import './App.css';
-import React from 'react';
 import Product from './components/Products';
 import Cart from './components/Cart';
+import Checkout from './components/Checkout';
 
 function App() {
+
+  const[cartItems, setCartItems] = useState([]);
+
+  function addToCart(product)
+  {
+    setCartItems(prevItems => [...prevItems, product]);
+  }
+
   return (
     <div className="App">
       <h1>Welcome to my e-commerce website</h1>
-      <Cart />
-      <Product />
+      <Cart cartItems={cartItems} />
+      <Product addToCart={addToCart} />
+      <Checkout cartItems={cartItems} />
     </div>
   );
 }
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,21 +1,12 @@
-import {useState} from 'react';
+
 
 // define cart component
-function Cart() 
+function Cart({cartItems}) 
 {
-    // declare a state variable and a function to update it
-    const [cartItems, setCartItems] = useState([]);
-
-    // function to add a product to cart
-    function addToCart(product) 
-    {
-        // update the cart items state by adding the product to the previous items array array
-        setCartItems(prevItems => [...prevItems, product]);
-    }
-
     // render the cart component, displaying a list of items in the cart
     return (
         <div className="cart">
+            <h3>Cart</h3>
             {cartItems.map(item => (
                 <div key={item.id} className="cart-item">
                     <h4>{item.title}</h4>
