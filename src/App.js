9dd8dfdb4b83ed47547b3084e9e128355100c5f9
Jonny Savastano <jonathan.savastano1246@gmail.com>
2024-04-06 10:08:59 -0600

    adding dom manipulation and breaking the app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Product from './components/Products';
 import Cart from './components/Cart';
 import Checkout from './components/Checkout';
+import { BrowserRouter as Router, RouterProvider, Switch, Link } from 'react-router-dom';
 
 function App() {
 
@@ -15,12 +16,20 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
       <h1>Welcome to my e-commerce website</h1>
-      <Cart cartItems={cartItems} />
-      <Product addToCart={addToCart} />
-      <Checkout cartItems={cartItems} />
+      <Link to="/cart">Go to Cart</Link>
+      <Switch>
+        <Route exact path="/">
+          <Product addToCart={addToCart} />
+        </Route>
+        <Route path="/cart">
+          <Cart cartItems={cartItems} />
+        </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
