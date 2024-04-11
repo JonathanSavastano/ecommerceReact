14437dac6c002ce7f8a6550dd1259147d373d9a3
Jonny Savastano <jonathan.savastano1246@gmail.com>
2024-04-10 18:22:02 -0600

    added item quantity counter to cart component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -6,13 +6,25 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 // define cart component
 function Cart({ cartItems, clearCart }) 
 {
+    // create a new array to group items by id and count
+    const groupedItems = cartItems.reduce((acc, item) => {
+        const existingItem = acc.find(i => i.id === item.id);
+        if (existingItem) {
+            existingItem.count += 1;
+        } else {
+            acc.push({...item, count: 1 });
+        }
+        return acc;
+    }, []);
+
     // render the cart component, displaying a list of items in the cart
     return (
         <div className="cart">
             <h3>Cart</h3>
-            {cartItems.map(item => (
+            {groupedItems.map(item => (
                 <div key={item.id} className="cart-item">
                     <h4>{item.title}</h4>
+                    {item.count > 1 && <p>Quantity: {item.count}</p>}
                     <img src={item.image} alt={item.title} style={{ width: '100px', height: '100px' }} />
                     <p>${item.price}</p>
                 </div>
